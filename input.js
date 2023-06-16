a32a03d6d6d5215dca51ff219741835ceaca523e
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-16 00:26:05 -0400

    lint code

--- input.js
+++ input.js
@@ -5,27 +5,27 @@ let connection;
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
-  };
+  }
   switch (key) {
-    case 'w':
-      connection.write('Move: up');
-      break;
-    case 'a':
-      connection.write('Move: left');
-      break;
-    case 's':
-      connection.write('Move: down');
-      break;
-    case 'd':
-      connection.write('Move: right');
-      break;
-    case 'l':
-      connection.write('Say: lolllz');
-      break;
-    case 'z':
-      connection.write('Say: snooozz');
-      break;
-  };
+  case 'w':
+    connection.write('Move: up');
+    break;
+  case 'a':
+    connection.write('Move: left');
+    break;
+  case 's':
+    connection.write('Move: down');
+    break;
+  case 'd':
+    connection.write('Move: right');
+    break;
+  case 'l':
+    connection.write('Say: lolllz');
+    break;
+  case 'z':
+    connection.write('Say: snooozz');
+    break;
+  }
 };
 
 const setupInput = function(conn) {
