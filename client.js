caaed7659128c882828650ff2b6fc8efcedfb30d
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-15 21:26:49 -0400

    set up name upon connection

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+const { Socket } = require('dgram');
 const net = require('net');
 
 const connect = function() {
@@ -9,7 +10,17 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // print out some information when making connection to the server
+  conn.on('connect', () => {
+    console.log('connection established');
+    //write out my initials
+    conn.write('Name: BYY');
+  });
+
+  //print out the data received from server
   conn.on('data', data => console.log(data));
+
+  
 
   return conn;
 };
